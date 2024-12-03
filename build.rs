70cbcf6a35c8795ed0646ca1ee2ba57e5f2de36d
nwiizo <syu.m.5151@gmail.com>
2024-12-03 19:12:09 +0900

    Bump version to 0.1.3 and update data file path

--- build.rs
+++ build.rs
@@ -6,11 +6,9 @@ use walkdir::WalkDir;
 
 fn main() {
     println!("cargo:rerun-if-changed=jpostcode-data/data/json");
-
     let json_dir = Path::new("jpostcode-data/data/json");
     let out_dir = std::env::var("OUT_DIR").unwrap();
     let dest_path = Path::new(&out_dir).join("address_data.json");
-
     let mut merged_data = HashMap::new();
 
     for entry in WalkDir::new(json_dir).into_iter().filter_map(|e| e.ok()) {
@@ -19,7 +17,6 @@ fn main() {
         {
             let content = fs::read_to_string(entry.path()).unwrap();
             let file_data: HashMap<String, Value> = serde_json::from_str(&content).unwrap();
-
             let prefix = entry.path().file_stem().unwrap().to_str().unwrap();
             for (suffix, data) in file_data {
                 let full_postcode = format!("{}{}", prefix, suffix);
@@ -27,6 +24,5 @@ fn main() {
             }
         }
     }
-
     fs::write(dest_path, serde_json::to_string(&merged_data).unwrap()).unwrap();
 }
